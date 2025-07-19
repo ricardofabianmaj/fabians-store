f73af825b36c5e71235246d1411ad177ace99468
Ricardo Fabian de Mello Albuquerque Junior <ricardofabian141@gmail.com>
2025-07-19 17:00:21 -0300

    Base of the project
    I has create the frontend and the backend part to start the project. After that i has installed all frontend dependecies, as Tailwind, Axios, Typescript...

---
+++ fabians-store/src/test/java/com/fabians/fabians_store/FabiansStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabians.fabians_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabiansStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
